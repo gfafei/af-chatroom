6cfeb96b63a4ac3952c5609cb65a7e2eaba8a5ca
afei <afei_gf@163.com>
2016-11-30 10:17:22 +0800

    changeDB

--- settings.js
+++ settings.js
@@ -3,6 +3,6 @@ var dev = process.env.NODE_ENV === 'dev';
 module.exports = {
   dev: dev,
   //数据库uri
-  db: dev ? 'mongodb://104.194.81.171/af-chat' : 'mongodb://127.0.0.1/af-chat2',
+  db: dev ? 'mongodb://104.194.81.171/af-chat2' : 'mongodb://127.0.0.1/af-chat',
   host: 'http://afei-gf-af-chatroom.daoapp.io',
 };
